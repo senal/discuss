0676afb820ea262c7699142bac663f63e6c432e3
Ranga Fernando <ranga.fernando@gmail.com>
2019-02-03 13:08:28 +1100

    Update layout to see the effect

--- config/dev.exs
+++ config/dev.exs
@@ -69,7 +69,7 @@ config :phoenix, :plug_init_mode, :runtime
 # Configure your database
 config :discuss, Discuss.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "RSforever",
   database: "discuss_dev",
   hostname: "localhost",
   pool_size: 10
